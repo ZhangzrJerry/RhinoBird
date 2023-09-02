1d98b7171ced7c1715c11092a7ee66eab1a428cc
ZhangzrJerry <212563134@qq.com>
2023-09-02 15:34:48 +0800

    202309021534

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-server = Flask(__name__)
+server = Flask(__name__, template_folder='templates')
 
 @server.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('html/login.html')
+    return render_template('templates/login.html')
 
 if __name__ == '__main__':
     server.run(debug=True)
