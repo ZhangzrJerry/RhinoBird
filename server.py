208b5b10525d6cf1703732a167169b6447099e95
ZhangzrJerry <212563134@qq.com>
2023-09-02 15:41:34 +0800

    templates/login.html fix renderer problem
    Signed-off-by: ZhangzrJerry <212563134@qq.com>

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ server = Flask(__name__, template_folder='templates')
 
 @server.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('templates/login.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     server.run(debug=True)
