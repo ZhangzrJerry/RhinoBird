cdd44efb7126cd4290fcb6214b8e7a2bcba0cfbd
notch15@outlook.com <LaLiberteGuidantLePeuple>
2023-09-09 19:30:05 +0800

    留下一堆bug，加油我的睿宗

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template
 from flask import redirect
 from flask import url_for
 from flask import request
+from collections import defaultdict
+import json
 import json, sys, os
 
 
@@ -56,12 +58,136 @@ def analysis():
 @app.route('/timeline', methods=['GET', 'POST'])
 def timeline():
     try:
-        data
+        data = [
+    {
+        "date": "2023-06-02",
+        "name": "百科全书",
+        "dura": 300,
+        "type": "百科"
+    },
+    {
+        "date": "2023-06-08",
+        "name": "孙子兵法",
+        "dura": 60,
+        "type": "军事"
+    },
+    {
+        "date": "2023-06-12",
+        "name": "梦里花落知多少",
+        "dura": 180,
+        "type": "小说"
+    },
+    {
+        "date": "2023-06-18",
+        "name": "论语",
+        "dura": 90,
+        "type": "哲学"
+    },
+    {
+        "date": "2023-06-22",
+        "name": "水浒传",
+        "dura": 240,
+        "type": "小说"
+    },
+    {
+        "date": "2023-06-28",
+        "name": "麦田里的守望者",
+        "dura": 150,
+        "type": "小说"
+    },
+    {
+        "date": "2023-07-04",
+        "name": "动物农场",
+        "dura": 120,
+        "type": "小说"
+    },
+    {
+        "date": "2023-07-10",
+        "name": "傲慢与偏见",
+        "dura": 210,
+        "type": "小说"
+    },
+    {
+        "date": "2023-07-16",
+        "name": "飘",
+        "dura": 300,
+        "type": "小说"
+    },
+    {
+        "date": "2023-08-03",
+        "name": "老人与海",
+        "dura": 90,
+        "type": "小说"
+    },
+    {
+        "date": "2023-08-09",
+        "name": "天龙八部",
+        "dura": 300,
+        "type": "小说"
+    },
+    {
+        "date": "2023-08-15",
+        "name": "金瓶梅",
+        "dura": 240,
+        "type": "小说"
+    },
+    {
+        "date": "2023-08-21",
+        "name": "茶花女",
+        "dura": 180,
+        "type": "小说"
+    },
+    {
+        "date": "2023-08-27",
+        "name": "西游记",
+        "dura": 270,
+        "type": "小说"
+    }
+]
+        
+        # timeline需要提供的数据：月份，本月总计时长，阅读最久的一本书，读的最多的一种类型
+        monthly_data = []
+        monthly_reading = defaultdict(lambda: {"total_duration": 0, "max_duration": 0, "max_duration_book": "", "type_counts": {},"max_type": "", "max_type_duration": 0})
+        for each_book in data:
+            date = each_book["date"]
+            month = int(date.split("-")[1].lstrip("0"))
+            duration = each_book["dura"]
+            book_name = each_book["name"]
+            book_type = each_book["type"]
+            # 计算特定月份总时长
+            monthly_reading[month]["total_duration"] += duration
+            # 超级抽象的排序
+            if duration > monthly_reading[month]["max_duration"]:
+                monthly_reading[month]["max_duration"] = duration
+                monthly_reading[month]["max_duration_book"] = book_name
+            if book_type not in monthly_reading[month]["type_counts"]:
+                monthly_reading[month]["type_counts"][book_type] = 0
+            # 处理type
+            monthly_reading[month]["type_counts"][book_type] += duration
+            max_type = max(monthly_reading[month]["type_counts"], key=lambda x: monthly_reading[month]["type_counts"][x])
+            monthly_reading[month]["max_type"] = max_type
+            monthly_reading[month]["max_type_duration"] = monthly_reading[month]["type_counts"][max_type]
+            # 将monthly_rading字典转换为JSON格式
+            # 这一块逻辑有点混乱，主要是我想把month塞进json里面
+        for month, info in monthly_reading.items():
+            max_type = max(info["type_counts"], key=lambda x: info["type_counts"][x])
+            max_type_duration = info["type_counts"][max_type]
+            monthly_data.append({
+                "month": month,
+                "total_duration": info["total_duration"],
+                "max_duration": info["max_duration"],
+                "max_duration_book": info["max_duration_book"],
+                "max_type": max_type,
+                "max_type_duration": max_type_duration
+            })
+        monthly_data_json = json.dumps(monthly_data, ensure_ascii=False, indent=4)
+        print(monthly_data_json)
+        return render_template('timeline.html', monthly_data_json=monthly_data_json)
+        
     except NameError:
         return redirect(url_for('login'))
     else:
-        return render_template('timeline.html', data=data)
-
+        return render_template('timeline.html', monthly_data_json=monthly_data_json)
 
 @app.route('/recommend', methods=['GET', 'POST'])
 def recommend():
